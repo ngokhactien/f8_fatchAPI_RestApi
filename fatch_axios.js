8e83afbeb6e7ebbb1a03e39f1ae67c17dd8db381
khactien <khactien9d@gmail.com>
2021-06-06 17:26:03 +0700

    fetch-axios-api

--- fatch_axios.js
+++ fatch_axios.js
@@ -1,3 +1,5 @@
+
+// fetch Api
 var post = 'http://localhost:5555/status';
 
 fetch(post).then( (respont)=>{
@@ -8,17 +10,33 @@ fetch(post).then( (respont)=>{
     alert(posts.name);
 });
 console.log('hello');
-// const axios = require('axios');
 
-// var post = 'http://localhost:5555/status';
+// axios
+const axios = require('axios');
 
-// axios.get(post).then( (res)=>{
-//     console.log('hello')
-//     console.log(res.data.name);
-// }).catch( (err)=>{
-//     console.log(err)
-// } )
+var post = 'http://localhost:5555/status';
 
+axios.get(post).then( (res)=>{
+    console.log('hello')
+    console.log(res.data.name);
+}).catch( (err)=>{
+    console.log(err)
+} )
+
+
+//isomorphic-fetch
+require('isomorphic-fetch');
+
+fetch('//offline-news-api.herokuapp.com/stories')
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    })
+    .then(function(stories) {
+        console.log(stories);
+    });
 
 
 
